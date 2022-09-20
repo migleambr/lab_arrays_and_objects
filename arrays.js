974c9e54080f10723e18aa1540981a990d601028
Migle Ambrazeviciute <ma320@st-andrews.ac.uk>
2022-09-20 16:56:45 +0300

    solved Q3

--- arrays.js
+++ arrays.js
@@ -18,8 +18,7 @@ stations.push("London Kings Cross");
  */
 
 stations.shift("Dundee");
-console.log(stations);
-
+// console.log(stations);
 
 
 /**
@@ -27,11 +26,8 @@ console.log(stations);
  * How many stations are on the line?
  */
 
-stationCount = null;
-
-// console.log("There are " + stationCount + " stations.");
-
-
+stationCount = stations.length;
+console.log("There are " + stationCount + " stations.");
 
 /**
  * Q4.
